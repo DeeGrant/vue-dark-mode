765e271eef7006e3a63289049d303bcb537292d3
Dee Grant <WalterGrant@gmail.com>
2022-07-10 11:57:02 -0500

    Add gui toggle and dark mode

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,61 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app" :class="mode">
+    <header-comp :mode="mode" @toggle="toggle"></header-comp>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderComp from "@/components/HeaderComp";
 
 export default {
   name: 'App',
+  data () {
+    return {
+      mode: 'light'
+    }
+  },
   components: {
-    HelloWorld
+    HeaderComp
+  },
+  methods: {
+    toggle() {
+      if(this.mode === 'dark') {
+        this.mode = 'light'
+      } else {
+        this.mode = 'dark'
+      }
+    }
   }
 }
 </script>
 
 <style>
-#app {
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
+
+.app {
+  width: 100vw;
+  min-height: 100vh;
+  background-color: #f3f3f3;
+  color: #15202b;
+  transition: background-color 0.3s ease-in-out;
+}
+
+.dark {
+  background-color: #192734;
+  color: #f3f3f3;
+}
+
+/*#app {*/
+/*  font-family: Avenir, Helvetica, Arial, sans-serif;*/
+/*  -webkit-font-smoothing: antialiased;*/
+/*  -moz-osx-font-smoothing: grayscale;*/
+/*  text-align: center;*/
+/*  color: #2c3e50;*/
+/*  margin-top: 60px;*/
+/*}*/
 </style>
---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,61 @@
+<template>
+<header>
+  <h1>{{mode}} Mode</h1>
+  <toggle-comp :mode="mode" @toggle="$emit('toggle')"></toggle-comp>
+  <nav>
+    <div class="nav-link">Home</div>
+    <div class="nav-link">About</div>
+    <div class="nav-link">Portfolio</div>
+    <div class="nav-link">Contact</div>
+  </nav>
+</header>
+</template>
+
+<script>
+import ToggleComp from "@/components/ToggleComp";
+
+export default {
+  name: "HeaderComp",
+  components: {ToggleComp},
+  props: ['mode']
+}
+</script>
+
+<style>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 25px;
+  height: 60px;
+  background-color: #c3c3c3;
+  transition: background-color 0.3s ease-in-out;
+}
+.dark header {
+  background-color: #15202b;
+}
+
+header h1 {
+  font-size: 32px;
+  text-transform: uppercase;
+  font-weight: 900;
+}
+
+header nav {
+  display: flex;
+  height: 60px;
+}
+
+header nav .nav-link {
+  display: flex;
+  align-items: center;
+  padding: 0 15px;
+}
+
+header nav .nav-link:hover {
+  background-color: #b0b0b0;
+}
+.dark header nav .nav-link:hover {
+  background-color: #101520;
+}
+</style>
